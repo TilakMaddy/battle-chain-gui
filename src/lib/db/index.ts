12a1963b0807a4baea42d7aeb5c72b5c7151f9f3
TilakMaddy <tilak.madichetti@gmail.com>
2026-02-24 15:07:28 +0530

    c

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,7 +1,7 @@
 import Database from "better-sqlite3";
 import { join } from "path";
 
-const DB_PATH = join(process.cwd(), "battlechain.db");
+const DB_PATH = join(process.cwd(), "db", "battlechain.db");
 
 let _db: Database.Database | null = null;
 
